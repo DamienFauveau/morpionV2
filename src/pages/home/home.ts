f96a5c16561b6407d5b854fc001becc3a84b1004
damienfauveau <damien.fauveau49@gmail.com>
2017-08-18 11:40:50 +0200

    2e

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,112 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	squares = Array(9).fill(null); //initialisation
+	player = 'X';
+	winner = null;
+	nb = 0;
 
-  }
+	constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+
+	}
+
+	get gameStatusMessage(){
+		if(this.winner) {
+			if(this.nb == 0) { // empeche boucle infinie
+				this.presentConfirm();
+				this.nb++;
+			}
+			return '"' + this.winner + '" a gagné !'; // si victoire
+		}
+		else {
+			if(this.cellsEmpty()) { // si il reste des cases
+				return 'Au tour de "' + this.player + '"';
+			}
+			if(this.nb == 0) { // empeche boucle infinie
+				this.presentConfirm();
+				this.nb++;
+			}
+			return 'Match nul'; // sinon match nul
+		}
+	}
+
+	presentConfirm() {
+		let alert = this.alertCtrl.create({ // quand la partie est finie
+			title: 'Partie terminée',
+			message: 'Voulez vous refaire une partie ?',
+			buttons: [
+			{
+				text: 'Annuler', // ne fait rien
+				role: 'cancel'
+			},
+			{
+				text: 'Recommencer', // reinitialise la partie
+				handler: () => {
+					this.restartGame();
+				}
+			}
+			]
+		});
+		alert.present();
+	}
+
+	handleMove(position) {
+		if(!this.winner && !this.squares[position] ) { // si pas de gagnant et pas deja rempli
+			this.squares[position] = this.player; // affecte la case au joueur
+			if(this.winnigMove()) { // si le point fait gagner
+				this.winner = this.player;
+			}
+			if (this.player == 'X') // sinon au tour du joueur suivant
+				this.player = 'O';
+			else
+				this.player = 'X';
+		}
+	}
+
+	iaTurn() {
+		let rnd = Math.floor((Math.random()*8));
+		if(this.cellsEmpty()) {
+			while(this.squares[rnd]) {
+				rnd = Math.floor((Math.random()*8));
+			}
+			setTimeout(() => this.handleMove(rnd), 500);
+		}
+	}
+
+	cellsEmpty() {
+		for(let i = 0 ; i < this.squares.length ; i++) { 
+			if(!this.squares[i]) { // si il reste des cases
+				return true;
+			}
+		}
+		return false;
+	}
+
+	winnigMove() { // conditions pour gagner
+		const conditions = [
+		[0, 1, 2], [3, 4, 5], [6, 7, 8], // lignes
+		[0, 3, 6], [1, 4, 7], [2, 5, 8], // colonnes
+		[0, 4, 8], [2, 4, 6]             // diagonales
+		];
+		for (let condition of conditions) { // verifie si un joueur a gagné
+			if ( this.squares[condition[0]] && this.squares[condition[0]] === this.squares[condition[1]] && this.squares[condition[1]] === this.squares[condition[2]]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	restartGame() {
+		this.squares = Array(9).fill(null); // reinitialisation
+		this.player = 'X';
+		this.winner = null;
+		this.nb = 0;
+	}
 
 }
